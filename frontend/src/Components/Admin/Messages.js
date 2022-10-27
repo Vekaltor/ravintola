4233ac1e7b32c71536b724d3b70bc0cd1a3a8c98
Vekaltor <56607344+Vekaltor@users.noreply.github.com>
2022-10-27 20:39:04 +0200

    Add Topbar + create content of dishes
    Get all dishes from API
    Made new options for topbar

---
+++ frontend/src/Components/Admin/Messages.js
@@ -0,0 +1,13 @@
+import { FiMessageSquare } from "react-icons/fi";
+
+function Messages() {
+  return (
+    <div className="messages">
+      <span className="icon">
+        <FiMessageSquare />
+      </span>
+    </div>
+  );
+}
+
+export default Messages;
---
+++ frontend/src/Components/Admin/Notifications.js
@@ -0,0 +1,13 @@
+import { MdNotificationsNone } from "react-icons/md";
+
+function Notifications() {
+  return (
+    <div className="notifications">
+      <span className="icon">
+        <MdNotificationsNone />
+      </span>
+    </div>
+  );
+}
+
+export default Notifications;
