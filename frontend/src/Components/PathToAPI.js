7bcb5a942124dcb78ef238eb4cec814b3f0d7bd5
Vekaltor <56607344+Vekaltor@users.noreply.github.com>
2022-11-10 14:39:25 +0100

    Set dynamic URL APP

--- frontend/src/Components/PathToAPI.js
+++ frontend/src/Components/PathToAPI.js
@@ -1,3 +1,7 @@
-export const pathToApi = "https://ravintola.herokuapp.com/";
+export const pathToApi =
+  process.env.NODE_ENV !== "development"
+    ? "https://ravintola.herokuapp.com/"
+    : "http://localhost:8080/";
+
 // http://localhost:8080/
 // https://ravintola.herokuapp.com/
