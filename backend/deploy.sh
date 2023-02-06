365490d5c99cf935346f5edeb0da910c70763f37
Vekaltor <56607344+Vekaltor@users.noreply.github.com>
2023-02-06 19:10:12 +0100

    Fixed path deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-cd C:/Users/Kamil/Desktop/ravintolaAPI-main/ravintola-app
+cd C:/Users/Kamil/Desktop/ravintolaAPI-main/ravintola-app/backend
 mvn clean install -DskipTests
 scp -P 10186 target/ravintolaAPI-0.0.1-SNAPSHOT.jar deploy@srv16.mikr.us:~/apps/ravintola
 ssh deploy@srv16.mikr.us -p 10186 'sudo systemctl restart ravintola.service'
